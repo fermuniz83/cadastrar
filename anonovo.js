cb19506954840db73ae6db7bec98d0e5d731d097
fermuniz83 <fercomobone@hotmail.com>
2021-12-07 22:07:39 -0300

    first commit

---
+++ anonovo.js
@@ -0,0 +1,21 @@
+const eventoAnoNovo ="01/01/2022"
+if (eventoAnoNovo <=05/01/2022){
+    console.log ("cadastro aprovado")
+} 
+else{
+    ("aguarde data para proximo evento")
+}
+
+const limiteDeIdade=15
+if(limiteDeIdade>=18){
+    console.log ("cadastro aprovado")
+}
+else{
+    ("cadastro negado")
+}
+const lotaçãoDoEvento=500
+if(lotaçãoDoEvento<=500){
+    console.log("cadastro efetuado com sucesso")
+}else{
+    console.log("evento esgotado")
+}
